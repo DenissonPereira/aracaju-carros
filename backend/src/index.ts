007fee5ce0bdb73a8e919d4004cb27af9f2cc702
Denisson Pereira Santos <denisson159@gmail.com>
2024-04-16 11:52:27 -0300

    feat: :lipstick: Add view

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from 'cors';
 import { routes } from "./routes";
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 dotenv.config();
 
@@ -11,9 +13,19 @@ const app = express();
 
 app.use(cors({
     origin: '*'
-}))
+}));
+
+app.set("view engine", "ejs"); 
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.set("views", path.join(__dirname, "views")); 
 
 app.use(express.json());
 app.use(routes);
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get("/", (req, res) => {
+    res.render("index"); 
+});
 
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
