77a4c1e3de8847a0c0c234bc46d4b59b65a5f726
Denisson Pereira Santos <denisson159@gmail.com>
2024-04-17 19:11:26 -0300

    feat: :lipstick: Update service component

--- mobile/src/screens/Servicos/index.tsx
+++ mobile/src/screens/Servicos/index.tsx
@@ -1,4 +1,5 @@
-import { SafeAreaView, Text, ImageBackground } from "react-native"
+import { SafeAreaView, Text, ImageBackground, Image, View } from "react-native"
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { styles } from "./styles"
 
 
@@ -8,7 +9,32 @@ export const Servicos = () => {
             style={[styles.safe, { overflow: 'hidden' }]}
             source={require('../../assets/images/servicos.png')}
         >
-
+            <View style={styles.view}>
+                <Image source={require('../..//assets/images/seguro.png')} style={styles.image} />
+            </View>
+            <View style={styles.textView}>
+                <Text style={styles.text}>Serviços</Text>
+            </View>
+            <View style={styles.linha}>
+                <View style={styles.container}>
+                    <View><MaterialCommunityIcons name="car-door" size={20} /></View>
+                    <Text>Funilaria</Text>
+                </View>
+                <View style={styles.container}>
+                    <View><MaterialCommunityIcons name="car-wash" size={20} /></View>
+                    <Text>Lavagem</Text>
+                </View>
+            </View>
+            <View style={styles.linha}>
+                <View style={styles.container}>
+                    <View><MaterialCommunityIcons name="car-multiple" size={20} /></View>
+                    <Text>Estética</Text>
+                </View>
+                <View style={styles.container}>
+                    <View><MaterialCommunityIcons name="car-wrench" size={20} /></View>
+                    <Text>Manutenção</Text>
+                </View>
+            </View>
         </ImageBackground>
     )
 }
--- mobile/src/screens/Servicos/styles.ts
+++ mobile/src/screens/Servicos/styles.ts
@@ -5,5 +5,39 @@ export const styles = StyleSheet.create({
     safe: {
         flex: 1,
         height: Dimensions.get("window").height,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20
+    },
+    image: {
+        width: 350,
+        height: 180,
+        borderRadius: 5,
+        marginTop: 320
+    },
+    view: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
+    linha: {
+        flexDirection: 'row',
+        gap: 20
+    },
+    container: {
+        backgroundColor: '#EBEBEB',
+        width: 160,
+        height: 80,
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textView: {
+        marginRight: 230
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: '500'
     }
 })
