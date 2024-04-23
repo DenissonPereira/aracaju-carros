4af8bc24a5f9ad3f890f2c2ebc58258ff983bbad
Denisson Pereira Santos <denisson159@gmail.com>
2024-04-23 12:53:28 -0300

    feat: :sparkles: Add cadastro na login page

--- web/src/pages/public/Login/components/Cabecario/index.tsx
+++ web/src/pages/public/Login/components/Cabecario/index.tsx
@@ -1,8 +1,10 @@
 import BemVindo from '../../../../../assets/images/bemvindo.png'
 
+import './cabecario.sass'
+
 export const Cabecario = () => {
   return (
-    <div>
+    <div className='cabecario_principal'>
         <figure>
             <img src={BemVindo} alt="Bem-vindo" />
         </figure>
--- web/src/pages/public/Login/index.tsx
+++ web/src/pages/public/Login/index.tsx
@@ -2,6 +2,11 @@ import { Cabecario, Contatos, FormularioLogin } from './components'
 import './login.sass'
 
 export const Login = () => {
+
+  const cadastrar = () => {
+
+  }
+
   return (
     <div className="login_principal">
       <div className="login_esquerdo">
@@ -9,6 +14,10 @@ export const Login = () => {
         <FormularioLogin />
         <div className="login_esquerdo_contatos">
           <Contatos />
+        </div>
+        <div className="login_esquerdo_criar_conta">
+          <p>Cadastre-se <a onClick={cadastrar}>aqui!</a></p>
+          <p><a href="">Esqueci a senha</a></p>
         </div>
         <div className="informacoes_direitos">
             <p>Todos os Direitos Reservados &copy; | 2024</p>
