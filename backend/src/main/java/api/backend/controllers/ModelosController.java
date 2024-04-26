91cf8ffbbb49714d93ea3dea087f1c87e648e140
Denisson Pereira Santos <denisson159@gmail.com>
2024-04-25 23:03:59 -0300

    feat: :alien: Add delete method modelo

--- backend/src/main/java/api/backend/controllers/ModelosController.java
+++ backend/src/main/java/api/backend/controllers/ModelosController.java
@@ -6,13 +6,17 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.backend.models.Modelos;
 import api.backend.repository.ModelosRepository;
+import api.backend.services.ModelosService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @AllArgsConstructor
@@ -20,6 +24,7 @@ public class ModelosController {
 
     @Autowired
     ModelosRepository modelosRepository;
+    ModelosService modelosService;
 
     @GetMapping("/modelos")
     public List<Modelos> getAllModelos() {
@@ -38,4 +43,27 @@ public class ModelosController {
                 .status(HttpStatus.NOT_FOUND)
                 .body("Modelo não encontrado!");
     }
+
+    @PostMapping("/modelos")
+    public ResponseEntity<?> createModelo(@RequestBody Modelos modelo) {
+
+        Modelos modeloSalvo = modelosService.cadastrarModelo(modelo);
+
+        return ResponseEntity.ok(modeloSalvo);
+    }
+
+    @DeleteMapping("/modelos/{id}")
+    public ResponseEntity<?> deleteModelo(@PathVariable Long id) {
+        Optional<Modelos> optionalModelo = modelosRepository.findById(id);
+
+        if (optionalModelo.isPresent()) {
+            modelosRepository.deleteById(id);
+            return ResponseEntity.ok().body("Deletado com sucesso!");
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Modelo não encontrado!");
+    }
+
 }
